e221c7096fd5c182622397b93f83fb33f17375c7
bumja12 <gngk1234@naver.com>
2022-09-16 01:55:33 +0900

    [ks44team04] 김범현 addressList 레이아웃 설정 및 데이터 설정 addressRegister 레이아웃 설정 및 데이터 입력 설정

--- ks44team04/src/main/java/ks44team04/user/controller/UserOrderController.java
+++ ks44team04/src/main/java/ks44team04/user/controller/UserOrderController.java
@@ -1,10 +1,9 @@
 package ks44team04.user.controller;
 
+import ks44team04.user.service.UserAddressService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user/order")
