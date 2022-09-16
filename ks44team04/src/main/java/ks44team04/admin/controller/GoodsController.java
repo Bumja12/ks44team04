51f6d8663816c906bb84e9f70fc2a663b43a81fa
inyoung <iyi-y@naver.com>
2022-09-16 17:58:16 +0900

    [team04] 관리자페이지-상품목록-수정페이지 만들기 author: 김인영 file: Goods.java / GoodsController.java / goodsList.html / GoodsMapper.java / GoodsMapper.xml / GoodsService.java / leftmenuin.html / modifyGoods.html detail: 진행중~ 수정 페이지가 오류~~....

--- ks44team04/src/main/java/ks44team04/admin/controller/GoodsController.java
+++ ks44team04/src/main/java/ks44team04/admin/controller/GoodsController.java
@@ -4,18 +4,40 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import ks44team04.admin.service.GoodsService;
 import ks44team04.dto.Goods;
 
 @Controller
-@RequestMapping("/admin/goods")
+@RequestMapping(value = "/admin/goods")
 public class GoodsController {
 	
 	//의존성 주입
 	private GoodsService goodsService;
 	public GoodsController(GoodsService goodsService) {
 		this.goodsService = goodsService;
+	}
+	
+	//상품 수정
+	@PostMapping("/modifyGoods")
+	public String modifyGoods(Goods goods) {
+		goodsService.modifyGoods(goods);
+		return "redirect:/goods/goodsList";
+	}
+	
+	//상품 수정	
+	@GetMapping("/modifyGoods")
+	public String modifyGoods(@RequestParam(value="goodsCode", required = false) String goodsCode, Model model) {
+		//특정회원의 정보
+		Goods goodsInfo = goodsService.getGoodsInfoByCode(goodsCode);
+		//model 셋팅
+		model.addAttribute("title", "상품수정");
+		model.addAttribute("goodsInfo", goodsInfo);
+		
+		return "goods/modifyGoods";
 	}
 
 	//상품 목록 조회
--- ks44team04/src/main/java/ks44team04/admin/service/GoodsService.java
+++ ks44team04/src/main/java/ks44team04/admin/service/GoodsService.java
@@ -10,10 +10,21 @@ import ks44team04.dto.Goods;
 @Service
 public class GoodsService {
 	
+	//의존성 주
     private final GoodsMapper goodsMapper;
-	
 	public GoodsService(GoodsMapper goodsMapper) {
 		this.goodsMapper = goodsMapper;
+	}
+	
+	//상품 수정
+	public void modifyGoods(Goods goods) {
+		goodsMapper.modifyGoods(goods);
+	}
+	
+	//특정상품 조회
+	public Goods getGoodsInfoByCode(String goodsCode) {
+		Goods goods = goodsMapper.getGoodsInfoByCode(goodsCode);
+		return goods;
 	}
 	
 	//상품 목록 조회
