637a161b157969fd67df0d35e36aa9ccd93e91e9
hch00 <hch00@192.168.219.108>
2022-09-19 02:07:19 +0900

    [team04] 신고 관리 - 신고리스트 author:황초희 file: reportList.html detail: 신고리스트

--- ks44team04/src/main/java/ks44team04/admin/service/UserService.java
+++ ks44team04/src/main/java/ks44team04/admin/service/UserService.java
@@ -2,7 +2,6 @@ package ks44team04.admin.service;
 
 import ks44team04.admin.mapper.UserMapper;
 import ks44team04.dto.Level;
-import ks44team04.dto.Report;
 import ks44team04.dto.User;
 
 import java.util.List;
@@ -66,11 +65,4 @@ public class UserService {
         return loginUserInfo;
     }
     
-    //신고 리스트 
-    public List<Report> getReportList(){
-    	List<Report> reportList = userMapper.getReportList();
-    	System.out.println("신고리스트");
-		return reportList;
-	}
-
 }
