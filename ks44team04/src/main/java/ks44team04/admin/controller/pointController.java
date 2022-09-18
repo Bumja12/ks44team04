4c6fda96172c349c51f7270139743797d9bf775c
greyco1or <greyco1or>
2022-09-19 03:14:10 +0900

    [ks44team04] 포인트페이지 추가
    author: 김석환
    file: leftmenu.html, leftmenusuk2.html, leftmenusuk.html, paymentDetail.html, pointController.java, pointHistory.html, leftmenusuk.html
    detail: 아이콘 수정, 포인트 페이지와 컨트롤러 추가, 결제상세내역 테이블 수정

---
+++ ks44team04/src/main/java/ks44team04/admin/controller/pointController.java
@@ -0,0 +1,19 @@
+package ks44team04.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class pointController {
+
+	    @GetMapping("/pointHistory")
+	    public String paymentList(Model model) {
+	    	
+	    	model.addAttribute("title", "포인트내역");
+	    	
+	        return "admin/pointHistory";
+	    }
+}
