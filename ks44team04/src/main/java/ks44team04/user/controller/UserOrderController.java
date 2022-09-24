c2ba49649e8ffac550010d6619642fb7efb16ded
bumja12 <gngk1234@naver.com>
2022-09-24 21:27:38 +0900

    [ks44team04] 김범현 체크아웃 페이지 ver1 ver2로 변경 예정

--- ks44team04/src/main/java/ks44team04/user/controller/UserOrderController.java
+++ ks44team04/src/main/java/ks44team04/user/controller/UserOrderController.java
@@ -1,15 +1,34 @@
 package ks44team04.user.controller;
 
+import ks44team04.dto.AddressList;
+import ks44team04.user.service.UserAddressService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user/order")
 public class UserOrderController {
 
+    UserAddressService userAddressService;
+
+    public UserOrderController(UserAddressService userAddressService) {
+        this.userAddressService = userAddressService;
+    }
+
     @GetMapping("/checkout")
-    public String checkout() {
+    public String checkout(Model model) {
+            String userId = "buyer01";
+            Map<String, String> addressInfo = new HashMap<>();
+            addressInfo.put("userId", userId);
+            List<AddressList> addressLists = userAddressService.getAddressList(addressInfo);
+            model.addAttribute("addressList", addressLists);
 
         return "user/order/checkout";
     }
