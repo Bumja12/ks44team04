a190b37351e77f75d47cc684021d7a3cfdc5a3ac
greyco1or <starbux630@naver.com>
2022-09-26 00:52:55 +0900

    [ks44team04] 포인트내역 데이터 받아오기
    author: 김석환
    file: couponList.html, PaymentMapper.xml, PointController.java, pointHistory.html, PointMapper.java, PointMapper.xml, PointService.java
    detail: 포인트내역 데이터를 받아서 화면에 출력하였다.

--- ks44team04/src/main/java/ks44team04/admin/controller/PointController.java
+++ ks44team04/src/main/java/ks44team04/admin/controller/PointController.java
@@ -1,19 +1,45 @@
 package ks44team04.admin.controller;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import ks44team04.admin.service.PointService;
+import ks44team04.dto.PointDeal;
+
 @Controller
 @RequestMapping("/admin")
 public class PointController {
-
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(PointController.class);
+	private final PointService pointService;
+	
+	@PostConstruct
+	public void pointControllerInit() {
+		log.info("PointController Bean 생성");
+	}
+	
+	public PointController(PointService pointService) {
+		
+		this.pointService = pointService;
+	}
+	
 	@GetMapping("/pointHistory")
 	public String paymentList(Model model) {
-
+		
+		List<PointDeal> pointHistory = pointService.pointHistory();
+		
 		model.addAttribute("title", "포인트내역");
-
+		model.addAttribute("pointHistory", pointHistory);
+		
 		return "admin/pointHistory";
 	}
 }
