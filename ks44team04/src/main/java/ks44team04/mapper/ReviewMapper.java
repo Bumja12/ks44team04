0090da295e307b735e1a54297ec1e2ccc4063f3c
chocho0j <chohee_44@naver.com>
2022-10-04 17:29:49 +0900

    [team04] 리뷰관리 - 리뷰 관리 author:황초희 file: ReviewController.java, reviewList.html, ReviewMapper.java, ReviewMapper.xml, ReviewService.java detail: 리뷰 화면 연결, 리스 보여주기 , 검색

--- ks44team04/src/main/java/ks44team04/mapper/ReviewMapper.java
+++ ks44team04/src/main/java/ks44team04/mapper/ReviewMapper.java
@@ -3,6 +3,7 @@ package ks44team04.mapper;
 
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
@@ -15,6 +16,8 @@ public interface ReviewMapper {
     //리뷰 리스트 
 	public List<Review> reviewList();
 	
+	//리뷰 검색
+	public List<Review> reviewSerchList(Map<String, Object> searchMap);
 	
 	
 
