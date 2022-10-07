8c09d1f2a75c1fba10d5fe14abc55eab25abd362
grey <starbux630@naver.com>
2022-10-07 12:45:43 +0900

    [ks44team04] 쿠폰발급 미완성. 체크박스 생성 author: 김석환 file: CouponController.java, couponStatus.html, couponList.html detail: 쿠폰발급 프로세스를 만들어야함

--- ks44team04/src/main/java/ks44team04/admin/controller/CouponController.java
+++ ks44team04/src/main/java/ks44team04/admin/controller/CouponController.java
@@ -10,9 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ks44team04.service.CouponService;
@@ -39,24 +37,32 @@ public class CouponController {
 		this.couponService = couponService;
 		this.userService = userService;
 	}
-	
+
+	//쿠폰발급
+	@GetMapping("/userList")
+	@ResponseBody
+	public List<User> getUserList(@RequestParam(value = "getUserListCheck") boolean getUserListCheck) {
+
+		if(getUserListCheck){
+			List<User> userList = userService.getUserList();
+			log.info("회원의 목록 ::: {}", userList);
+
+			return userList;
+
+		}else {
+
+			return null;
+		}
+	}
+
+	//쿠폰생성
 	@PostMapping("/couponCreate")
-	public String CouponCreate(Coupon coupon
-								,RedirectAttributes reAttr) {
+	public String CouponCreate(Coupon coupon) {
 
 		System.out.println(coupon);
 		couponService.couponCreate(coupon);
 
 		return "redirect:/admin/coupon/couponList";
-	}
-
-	@GetMapping("/userList")
-	public String getUserList(Model model) {
-		List<User> userList = userService.getUserList();
-		log.info("회원의 목록 ::: {}", userList);
-		model.addAttribute("userList", userList);
-
-		return "admin/user/userList";
 	}
 
 	//쿠폰보유현황
