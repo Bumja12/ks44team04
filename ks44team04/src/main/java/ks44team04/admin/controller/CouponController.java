f3642a9b011b97873a128294fc0328859e290b42
grey <starbux630@naver.com>
2022-10-11 16:35:20 +0900

    [ks44team04] 쿠폰 검색기능 완성, 쿠폰발급은 미완성 author: 김석환 file: CouponController.java       couponList.html       couponListSearch.html       CouponMapper.java       CouponMapper.xml       CouponService.java       couponStatus.html       couponStatusSearch.html       misc.xml detail: 쿠폰발급은 미완성 여러모로 확인해봐야함

--- ks44team04/src/main/java/ks44team04/admin/controller/CouponController.java
+++ ks44team04/src/main/java/ks44team04/admin/controller/CouponController.java
@@ -1,9 +1,12 @@
 package ks44team04.admin.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 
+import ks44team04.dto.PaymentTotal;
 import ks44team04.dto.User;
 import ks44team04.service.UserService;
 import org.slf4j.Logger;
@@ -32,12 +35,71 @@ public class CouponController {
 		
 		log.info("couponController Bean 생성");
 	}
-	
+
 	public CouponController(CouponService couponService, UserService userService) {
 		this.couponService = couponService;
 		this.userService = userService;
 	}
 
+	//쿠폰보유 검색
+	@PostMapping("/couponStatus")
+	public String searchCouponStatus(@RequestParam(name="searchKey", defaultValue = "couponName") String sk
+			,@RequestParam(name="searchValue", required = false, defaultValue = "") String sv
+			,@RequestParam(name="fromDate", required = false, defaultValue= "") String fromDate
+			,@RequestParam(name="toDate", required = false, defaultValue= "") String toDate
+			,Model model) {
+
+		if(sk.equals("buyerId")) {
+			sk = "buyer_id";
+		}else if(sk.equals("couponCode")) {
+			sk = "coupon_code";
+		}else if(sk.equals("couponName")) {
+			sk = "coupon_name";
+		}
+
+		Map<String, Object> searchMap = new HashMap<String, Object>();
+		searchMap.put("sk", sk);
+		searchMap.put("sv", sv);
+		searchMap.put("fd", fromDate);
+		searchMap.put("td", toDate);
+
+		List<Coupon> couponStatus = couponService.searchCouponStatus(searchMap);
+		model.addAttribute("title", "검색결과");
+		model.addAttribute("couponStatus", couponStatus);
+
+		return "admin/coupon/couponStatus";
+	}
+
+	//쿠폰목록검색
+	@PostMapping("/couponList")
+	public String searchCouponList(@RequestParam(name="searchKey", defaultValue = "couponName") String sk
+			,@RequestParam(name="searchValue", required = false, defaultValue = "") String sv
+			,@RequestParam(name="fromDate", required = false, defaultValue= "") String fromDate
+			,@RequestParam(name="toDate", required = false, defaultValue= "") String toDate
+			,Model model) {
+
+		if(sk.equals("couponCode")) {
+			sk = "coupon_code";
+		}else if(sk.equals("couponName")) {
+			sk = "coupon_name";
+		}else if(sk.equals("discountPrice")) {
+			sk = "discount_price";
+		}
+
+		Map<String, Object> searchMap = new HashMap<String, Object>();
+		searchMap.put("sk", sk);
+		searchMap.put("sv", sv);
+		searchMap.put("fd", fromDate);
+		searchMap.put("td", toDate);
+
+		List<Coupon> couponList = couponService.searchCouponList(searchMap);
+		model.addAttribute("title", "검색결과");
+		model.addAttribute("couponList", couponList);
+
+		return "admin/coupon/couponList";
+	}
+
+	//쿠폰발급
 	@PostMapping("/couponIssue")
 	public String CouponIssue(@RequestParam List<String> userId) {
 
@@ -52,7 +114,7 @@ public class CouponController {
 		return "redirect:/admin/coupon/couponStatus";
 	}
 
-	//쿠폰발급
+	//유저목록 불러오기
 	@GetMapping("/userList")
 	@ResponseBody
 	public List<User> getUserList(@RequestParam(value = "getUserListCheck") boolean getUserListCheck) {
@@ -88,7 +150,7 @@ public class CouponController {
 		model.addAttribute("couponStatus", couponStatus);
 		System.out.println(couponStatus);
 		
-		return "/admin/coupon/couponStatus";
+		return "admin/coupon/couponStatus";
 	}
 	
 	//쿠폰목록
@@ -100,7 +162,7 @@ public class CouponController {
 		model.addAttribute("title", "쿠폰목록");
 		model.addAttribute("couponList", couponList);
 		
-		return "/admin/coupon/couponList";
+		return "admin/coupon/couponList";
 	}
 	
 }
