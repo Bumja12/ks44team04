30ab6f4018ce3c37dda0089d6279b625c8215c57
oasis16z <oasis16z@naver.com>
2022-10-11 12:34:31 +0900

    [team04]
    Author : 김선규
    Detail : 카테고리 관리 기능 개편 작업 중

--- ks44team04/src/main/java/ks44team04/admin/controller/CategoryController.java
+++ ks44team04/src/main/java/ks44team04/admin/controller/CategoryController.java
@@ -55,12 +55,83 @@ public class CategoryController {
 		model.addAttribute("title", "카테고리 등록 화면");
 		return "admin/category/category_add";
 	}
+	
+	// --------------------------------------카테고리 목록  화면--------------------------------------
+	
+	// 구매자 등급 카테고리 목록 화면
+	@GetMapping("/levelBuyer")
+	public String getlevelBuyerCategoryList(Model model) {
 
-	// 카테고리 목록 화면
-	@GetMapping("/categoryList")
-	public String getCategoryList(Model model) {
-
-		model.addAttribute("title", "카테고리 목록 화면");
-		return "admin/category/category_list";
+		model.addAttribute("title", "구매자 등급 카테고리 목록 화면");
+		return "admin/category/levelBuyer/levelBuyerCategory_list";
 	}
+	
+	// 판매자 등급 카테고리 목록 화면
+	@GetMapping("/levelSeller")
+	public String getlevelSellerCategoryList(Model model) {
+		
+		model.addAttribute("title", "판매자 등급 카테고리 목록 화면");
+		return "admin/category/levelSeller/levelSellerCategory_list";
+	}
+	
+	// 고객 문의 카테고리 목록 화면
+	@GetMapping("/customerAsk")
+	public String getCustomerAskCategoryList(Model model) {
+		
+		model.addAttribute("title", "고객 문의 카테고리 목록 화면");
+		return "admin/category/customerAsk/customerAskCategory_list";
+	}
+	
+	// 자주 묻는 질문 카테고리 목록 화면
+	@GetMapping("/regularAsk")
+	public String getRegularAskCategoryList(Model model) {
+		
+		model.addAttribute("title", "자주 묻는 질문 카테고리 목록 화면");
+		return "admin/category/regularAsk/regularAskCategory_list";
+	}
+	
+	// 게시판 카테고리 목록 화면
+	@GetMapping("/board")
+	public String getBoardCategoryList(Model model) {
+		
+		model.addAttribute("title", "게시판 카테고리 목록 화면");
+		return "admin/category/board/boardCategory_list";
+	}
+	
+	// 신고 대상 카테고리 목록 화면
+	@GetMapping("/report")
+	public String getReportCategoryList(Model model) {
+		
+		model.addAttribute("title", "신고 대상 카테고리 목록 화면");
+		return "admin/category/report/reportCategory_list";
+	}
+	
+	// 판매 상품 카테고리 대분류 목록 화면
+	@GetMapping("/goodsLarge")
+	public String getGoodsLargeCategoryList(Model model) {
+		
+		model.addAttribute("title", "판매 상품 카테고리 대분류 목록 화면");
+		return "admin/category/goodsLarge/goodsLargeCategory_list";
+	}
+	
+	// 판매 상품 카테고리 소분류 목록 화면
+	@GetMapping("/goodsSmall")
+	public String getGoodsSmallCategoryList(Model model) {
+		
+		model.addAttribute("title", "판매 상품 카테고리 소분류 목록 화면");
+		return "admin/category/goodsSmall/goodsSmallCategory_list";
+	}
+	
+	// 상품 문의 카테고리 목록 화면
+	@GetMapping("/goodsQna")
+	public String getGoodsQnaCategoryList(Model model) {
+		
+		model.addAttribute("title", "상품 문의 카테고리 목록 화면");
+		return "admin/category/goodsQna/goodsQnaCategory_list";
+	}
+	
+	
+	// --------------------------------------카테고리 목록  화면--------------------------------------
+	
+	
 }
